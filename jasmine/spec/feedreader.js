6c576b94d9a80a393a10ca542525d4306f68ca98
Micah Yunana Bala <micahkulien@gmail.com>
2018-09-15 18:21:18 +0100

    feed name is defined and not empty

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -42,6 +42,12 @@ $(
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
+      it('has a name defined', function() {
+        allFeeds.forEach(feed => {
+          expect(feed.name).toBeDefined();
+          expect(feed.name.length).not.toBe(0);
+        });
+      });
     });
 
     /* TODO: Write a new test suite named "The menu" */
